19d7cea264479a767b48ebe324f5ba18127cb094
elioenaiferrari <elioenaiferrari@gmail.com>
2020-10-11 15:08:39 -0300

    base

---
+++ src/components/Signin.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-col cols="4" align="right">
+    <v-img
+      contain
+      class="mb-10"
+      max-width="200"
+      :src="require('../assets/images/logo.png')"
+    />
+    <v-form ref="form" :valid="valid">
+      <v-text-field
+        outlined
+        v-for="field in fields"
+        :key="field.key"
+        :label="field.label"
+        v-model="field.value"
+        :rules="field.rules"
+        color="accent"
+        @input="validate()"
+        required
+      />
+    </v-form>
+    <v-btn @click="signin()" block contained color="accent" :disabled="!valid">
+      ENTRAR
+    </v-btn>
+  </v-col>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: false,
+    fields: [
+      {
+        key: 'email',
+        label: 'E-mail',
+        value: '',
+        rules: [
+          (v) => !!v || 'E-mail é obrigatório',
+          (v) => /.+@.+/.test(v) || 'E-mail inválido',
+        ],
+      },
+      {
+        key: 'password',
+        label: 'Senha',
+        value: '',
+        rules: [(v) => !!v || 'Senha é obrigatória'],
+      },
+    ],
+  }),
+
+  methods: {
+    validate() {
+      if (this.$refs.form.validate()) {
+        this.valid = true;
+      }
+    },
+
+    signin() {},
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Signup.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-col
+    justify="center"
+    align="right"
+    cols="7"
+    class="accent rounded-lg ml-10 pa-5"
+  >
+    <v-form ref="form" :valid="valid">
+      <v-row>
+        <v-col cols="6" v-for="field in fields" :key="field.key">
+          <v-text-field
+            outlined
+            :label="field.label"
+            v-model="field.value"
+            :rules="field.rules"
+            color="white"
+            required
+            @input="validate()"
+          />
+        </v-col>
+        <v-col cols="6">
+          <v-btn
+            x-large
+            block
+            outlined
+            color="white"
+            @click="signup()"
+            :disabled="!valid"
+          >
+            CADASTRAR
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
+  </v-col>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: false,
+    fields: [
+      {
+        key: 'email',
+        label: 'E-mail',
+        value: '',
+        rules: [
+          (v) => !!v || 'E-mail é obrigatório',
+          (v) => /.+@.+/.test(v) || 'E-mail inválido',
+        ],
+      },
+      {
+        key: 'name',
+        label: 'Nome',
+        value: '',
+        rules: [(v) => !!v || 'Nome é obrigatório'],
+      },
+      {
+        key: 'password',
+        label: 'Senha',
+        value: '',
+        rules: [(v) => !!v || 'Senha é obrigatória'],
+      },
+    ],
+  }),
+
+  methods: {
+    validate() {
+      if (this.$refs.form.validate()) {
+        this.valid = true;
+      }
+    },
+
+    signup() {},
+  },
+};
+</script>
+
+<style></style>
